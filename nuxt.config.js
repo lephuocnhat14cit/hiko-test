7f5effe7da0923febddeb9fd9f1d060fa795181a
lephuocnhat.14cit <lephuocnhat.14cit@gmail.com>
2022-06-03 22:21:41 +0700

    config build

--- nuxt.config.js
+++ nuxt.config.js
@@ -57,6 +57,9 @@ export default {
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {
+    babel: {
+      compact: true,
+     },
   },
   i18n: {
     locales: ["vi", "en"],
