f37e48a935cec1a2b20ddbf57e256665ca909e3b
lephuocnhat.14cit <lephuocnhat.14cit@gmail.com>
2022-06-03 20:59:59 +0700

    update template

--- components/aboutUs.vue
+++ components/aboutUs.vue
@@ -68,6 +68,18 @@
         </div>
       </div>
     </div>
+    <div class="wrapper-animation">
+      <div class="Illustrator-container">
+        <img
+          class="Illustrator"
+          src="../assets/images/contents/Illustrator.png"
+          alt="Illustrator"
+        />
+      </div>
+      <div class="map-wrapper">
+        <img class="map" src="../assets/images/contents/map.svg" alt="Map" />
+      </div>
+    </div>
   </section>
 </template>
 
--- pages/index.vue
+++ pages/index.vue
@@ -2,39 +2,26 @@
   <div>
     <Header />
     <AboutUs />
-    <AnimationArea />
     <OurGames />
     <OurPartners />
-    <ContactUs />
     <Footer />
   </div>
 </template>
-
 <script>
-import Tutorial from '../components/Tutorial.vue';
 import Header from '../components/header.vue';
-import AnimationArea from '@/components/animationArea.vue';
 import OurGames from '@/components/ourGames.vue';
 import OurPartners from '@/components/ourPartners.vue';
-import ContactUs from '@/components/contactUs.vue';
 import Footer from '@/components/footer.vue';
 import AboutUs from '@/components/aboutUs.vue'
 export default {
   name: "IndexPage",
   components: {
-    Tutorial,
     Header,
-    AnimationArea,
+    AboutUs,
     OurGames,
     OurPartners,
-    ContactUs,
-    Footer,
-    AboutUs
+    Footer
   },
-  methods: {
-    handleClick () {
-      console.log('hahahah');
-    }
-  }  
 }
 </script>
+``
